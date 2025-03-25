b5fd54ae1b104204110803a8776c7e50ebf05109
Destinee A. Adams <adestinee37@gmail.com>
2025-03-24 23:14:03 -0400

    re wrote js

--- main.js
+++ main.js
@@ -1,68 +1,64 @@
+let list = {
+    nature: [
+        "parks you have been to",
+        "beaches you have been to",
+        "trees",
+        "animals",
+        "fall",
+        "winter",
+        "summer",
+        "spring",
+        "your favorite season",
+        "wildlife you've seen in your area",
+        "cloud shapes you've seen",
+        "the climate in your regions"
 
-function generate() {
-    let list = {
-        nature: [
-            "parks you have been to",
-            "beaches you have been to",
-            "trees",
-            "animals",
-            "fall",
-            "winter",
-            "summer",
-            "spring",
-            "your favorite season",
-            "wildlife you've seen in your area",
-            "cloud shapes you've seen",
-            "the climate in your regions"
+    ],
 
-        ],
-    
-        thing: [
-            "photos",
-            "drawings",
-            "poem(s)",
-            "maps"
-        ],
-    
-        experience: [
-            "experience",
-            "feelings",
-            "day"
-        ],
-    
-        page: [
-            "four",
-            "two",
-            "ten",
-            "eight",
-            "fourteen",
-            "twelve",
-            "sixteen"
-        ]
-    }
-    
-    let pages,nature,thing1, thing2,experience = null;
-    
-    
-    pages = list.page[getRandInt(list.page)];
-    nature = list.nature[getRandInt(list.nature)];
-    thing1 = list.thing[getRandInt(list.thing)];
-    thing2 = list.thing[getRandInt(list.thing)];
-    experience = list.experience[getRandInt(list.experience)];
-    
-    
+    thing: [
+        "photos",
+        "drawings",
+        "poem(s)",
+        "maps"
+    ],
 
-    var element = document.getElementById("idea");
-    element.innerHTML =  `Make a  ${pages} page zine about ${nature} with ${thing1} and ${thing2} to capture the ${experience} you had.`;
+    experience: [
+        "experience",
+        "feelings",
+        "day"
+    ],
+
+    page: [
+        "four",
+        "two",
+        "ten",
+        "eight",
+        "fourteen",
+        "twelve",
+        "sixteen"
+    ],
     
-    
+    getContent: () => {
+        pages = list.page[getRandInt(list.page)];
+        nature = list.nature[getRandInt(list.nature)];
+        thing1 = list.thing[getRandInt(list.thing)];
+        thing2 = list.thing[getRandInt(list.thing)];
+        experience = list.experience[getRandInt(list.experience)];
+        
+        return `Make a  ${pages} page zine about ${nature} with ${thing1} and ${thing2} to capture the ${experience} you had.`;
+    }  
 }
-
-
-
 
 
 function getRandInt(listName) {
     let num = Math.floor(Math.random() * listName.length);
     return num;
 }
+
+
+
+document.querySelector('button').addEventListener('click', () => {
+    document.querySelector("p").innerText = list.getContent();
+    console.log("test");
+    
+});
